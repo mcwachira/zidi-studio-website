ca9ffe36b493fc2f934d190532e155960a21e7a9
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-07-05 18:41:07 +0300

    Update Blog.tsx

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -50,27 +50,6 @@ const Blog = () => {
                             image="https://i.ibb.co/7jdcnwn/image-03.jpg"
                         />
                     </div>
-                </div>
-            </section>
-
-            <section className="pt-20 pb-10 lg:pt-[120px] lg:pb-20">
-                <div className="container">
-                    <div className="flex flex-wrap -mx-4">
-                        <div className="w-full px-4">
-                            <div className="mx-auto mb-[60px] max-w-[510px] text-center lg:mb-20">
-                <span className="block mb-2 text-lg font-semibold text-primary">
-                  Our Blogs
-                </span>
-                                <h2 className="mb-4 text-3xl font-bold text-dark sm:text-4xl md:text-[40px]">
-                                    Our Recent News
-                                </h2>
-                                <p className="text-base text-body-color">
-                                    There are many variations of passages of Lorem Ipsum available
-                                    but the majority have suffered alteration in some form.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
 
                     <div className="flex flex-wrap -mx-4">
                         <BlogCard
@@ -94,6 +73,8 @@ const Blog = () => {
                     </div>
                 </div>
             </section>
+
+           
         </>
     );
 };
